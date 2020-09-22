8a1d134969ffa3edbfb5bee184f82f9df367e798
GustavoBonfimS <gustavo.hbonfim@outlook.com>
2020-09-22 13:54:26 -0300

    cardapio: carrinho, cheklistbox item check

--- frmCardapio.cs
+++ frmCardapio.cs
@@ -4,17 +4,68 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Linq;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 
 namespace OrderPizza
 {
     public partial class frmCardapio : Form
     {
-        private List<Produto> checkedPizzaas = new List<Produto>();
         private ProdutoDAO produtoDAO = new ProdutoDAO();
+        List<Produto> produtos;
+        ObservableCollection<Produto> carrinho;
+
         public frmCardapio()
         {
-            InitializeComponent();
             this.Icon = new Icon("pizza.ico");
+            InitializeComponent();
+
+            produtos = new List<Produto>();
+            carrinho = new ObservableCollection<Produto>();
+            this.produtos = this.produtoDAO.listProdutos();
+
+            carrinho.CollectionChanged += carrinho_CollectionChanged;
+        }
+
+        private void frmCardapio_Load(object sender, EventArgs e)
+        {
+            cbPesquisar.SelectedItem = null;
+            setProdutos();
+
+        }
+
+        private void carrinho_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            double valorTotal = 0;
+            foreach (var item in carrinho)
+            {
+                valorTotal += item.preco;
+            }
+            lbTotal.Text = valorTotal.ToString("N2");
+        }
+
+        private void btnProximo_Click(object sender, EventArgs e)
+        {
+            foreach (var item in carrinho)
+            {
+                MessageBox.Show(item.nome + item.preco.ToString());
+            }
+        }
+
+        private void checkedPizzas_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            this.SetItemsCheckOnCarrinho(checkedPizzas, e);
+        }
+
+        private void checkedDoces_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            this.SetItemsCheckOnCarrinho(checkedDoces, e);
+        }
+
+        private void checkedBebidas_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            this.SetItemsCheckOnCarrinho(checkedBebidas, e);
         }
 
         private void btnMaximizar_Click(object sender, EventArgs e)
@@ -33,15 +84,8 @@ namespace OrderPizza
             this.WindowState = FormWindowState.Minimized;
         }
 
-        private void frmCardapio_Load(object sender, EventArgs e)
-        {
-            cbPesquisar.SelectedItem = null;
-            setProdutos();
-        }
-
         private void setProdutos()
         {
-            List<Produto> produtos = this.produtoDAO.listProdutos();
             checkedPizzas.Items.Clear();
             checkedDoces.Items.Clear();
             checkedDoces.Items.Clear();
@@ -56,17 +100,24 @@ namespace OrderPizza
                         checkedDoces.Items.Add(item.nome);
                         break;
                     case "bebida":
-                        checkedBebidaas.Items.Add(item.nome);
+                        checkedBebidas.Items.Add(item.nome);
                         break;
                 }
             }
         }
 
-        private void checkedPizzas_SelectedValueChanged(object sender, EventArgs e)
+        private void SetItemsCheckOnCarrinho(CheckedListBox list, ItemCheckEventArgs e)
         {
-            foreach (var item in checkedPizzas.CheckedItems)
+            if (list.GetItemCheckState(e.Index) == CheckState.Unchecked)
             {
-                // retorna os items que estao checkados          
+                // foi marcado agora
+                this.carrinho.Add(this.produtos.Where(prod => prod.nome == list.Items[e.Index].ToString()).ToArray()[0]);
+            }
+
+            if (list.GetItemCheckState(e.Index) == CheckState.Checked)
+            {
+                // foi desmarcado agora
+                this.carrinho.Remove(this.produtos.Where(prod => prod.nome == list.Items[e.Index].ToString()).ToArray()[0]);
             }
         }
     }
