0d3b321020b0cb5dcfa63064d7d9ed5eb9521b1b
Gustavo Henrique Bonfim <gustavo.hbonfim@outlook.com>
2020-09-23 18:12:38 -0300

    tela do cliente (#2)
    Co-authored-by: Felipe1540 <Felipe_1540@outlook.com>

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace frmCadCliente
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnMaximizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbxTelefone_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
