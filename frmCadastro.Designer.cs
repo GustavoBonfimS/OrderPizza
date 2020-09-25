3fd9159c84f7916f5dba84009ab76ea8b6485eb3
Luis Carlos <luisinho0903@gmail.com>
2020-09-25 20:28:31 -0300

    Pronto seu corno ta commitado arrombado enfia no cu

---
+++ frmCadastro.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace OrderPizza
+{
+    partial class frmAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.txbDescricao = new System.Windows.Forms.TextBox();
+            this.txbPreco = new System.Windows.Forms.TextBox();
+            this.txbNome = new System.Windows.Forms.TextBox();
+            this.txbTipo = new System.Windows.Forms.TextBox();
+            this.txbTamanho = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txbTipoUsu = new System.Windows.Forms.TextBox();
+            this.txbSenha = new System.Windows.Forms.TextBox();
+            this.txbLogin = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnCadFunc = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(47, 251);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCadastrar.TabIndex = 0;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // txbDescricao
+            // 
+            this.txbDescricao.Location = new System.Drawing.Point(36, 81);
+            this.txbDescricao.Name = "txbDescricao";
+            this.txbDescricao.Size = new System.Drawing.Size(100, 20);
+            this.txbDescricao.TabIndex = 1;
+            // 
+            // txbPreco
+            // 
+            this.txbPreco.Location = new System.Drawing.Point(36, 123);
+            this.txbPreco.Name = "txbPreco";
+            this.txbPreco.Size = new System.Drawing.Size(100, 20);
+            this.txbPreco.TabIndex = 2;
+            // 
+            // txbNome
+            // 
+            this.txbNome.Location = new System.Drawing.Point(36, 41);
+            this.txbNome.Name = "txbNome";
+            this.txbNome.Size = new System.Drawing.Size(100, 20);
+            this.txbNome.TabIndex = 3;
+            // 
+            // txbTipo
+            // 
+            this.txbTipo.Location = new System.Drawing.Point(36, 168);
+            this.txbTipo.Name = "txbTipo";
+            this.txbTipo.Size = new System.Drawing.Size(100, 20);
+            this.txbTipo.TabIndex = 4;
+            // 
+            // txbTamanho
+            // 
+            this.txbTamanho.Location = new System.Drawing.Point(36, 215);
+            this.txbTamanho.Name = "txbTamanho";
+            this.txbTamanho.Size = new System.Drawing.Size(100, 20);
+            this.txbTamanho.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Descrição";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Preço";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Tipo";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 199);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Tamanho";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.btnCadastrar);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txbDescricao);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txbPreco);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txbNome);
+            this.panel1.Controls.Add(this.txbTipo);
+            this.panel1.Controls.Add(this.txbTamanho);
+            this.panel1.Location = new System.Drawing.Point(25, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(218, 313);
+            this.panel1.TabIndex = 11;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txbTipoUsu);
+            this.panel2.Controls.Add(this.txbSenha);
+            this.panel2.Controls.Add(this.txbLogin);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.btnCadFunc);
+            this.panel2.Location = new System.Drawing.Point(599, 27);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(156, 212);
+            this.panel2.TabIndex = 12;
+            // 
+            // txbTipoUsu
+            // 
+            this.txbTipoUsu.Location = new System.Drawing.Point(28, 123);
+            this.txbTipoUsu.Name = "txbTipoUsu";
+            this.txbTipoUsu.Size = new System.Drawing.Size(100, 20);
+            this.txbTipoUsu.TabIndex = 17;
+            // 
+            // txbSenha
+            // 
+            this.txbSenha.Location = new System.Drawing.Point(28, 81);
+            this.txbSenha.Name = "txbSenha";
+            this.txbSenha.Size = new System.Drawing.Size(100, 20);
+            this.txbSenha.TabIndex = 16;
+            // 
+            // txbLogin
+            // 
+            this.txbLogin.Location = new System.Drawing.Point(28, 41);
+            this.txbLogin.Name = "txbLogin";
+            this.txbLogin.Size = new System.Drawing.Size(100, 20);
+            this.txbLogin.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(62, 107);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(28, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Tipo";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(62, 65);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Senha";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(62, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Login";
+            // 
+            // btnCadFunc
+            // 
+            this.btnCadFunc.Location = new System.Drawing.Point(40, 149);
+            this.btnCadFunc.Name = "btnCadFunc";
+            this.btnCadFunc.Size = new System.Drawing.Size(75, 23);
+            this.btnCadFunc.TabIndex = 13;
+            this.btnCadFunc.Text = "Cadastrar";
+            this.btnCadFunc.UseVisualStyleBackColor = true;
+            this.btnCadFunc.Click += new System.EventHandler(this.btnCadFunc_Click);
+            // 
+            // frmAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmAdmin";
+            this.Text = "frmAdmin";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.TextBox txbDescricao;
+        private System.Windows.Forms.TextBox txbPreco;
+        private System.Windows.Forms.TextBox txbNome;
+        private System.Windows.Forms.TextBox txbTipo;
+        private System.Windows.Forms.TextBox txbTamanho;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txbTipoUsu;
+        private System.Windows.Forms.TextBox txbSenha;
+        private System.Windows.Forms.TextBox txbLogin;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnCadFunc;
+    }
+}
---
+++ frmCadastro.cs
@@ -0,0 +1,92 @@
+﻿using OrderPizza.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace OrderPizza
+{
+    public partial class frmAdmin : Form
+    {
+        public frmAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            String nome;
+            String descricao;
+            double preco;
+            String tipo;
+            String tamanho;
+            if (String.IsNullOrEmpty(txbDescricao.Text) ||
+                String.IsNullOrEmpty(txbNome.Text) ||
+                String.IsNullOrEmpty(txbPreco.Text) ||
+                String.IsNullOrEmpty(txbTamanho.Text) ||
+                String.IsNullOrEmpty(txbTipo.Text))
+            {
+                MessageBox.Show("Um dos Campos exigidos está vazio!!", "Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            else
+            {
+                nome = txbNome.Text;
+                descricao = txbDescricao.Text;
+                preco = Convert.ToDouble(txbPreco.Text);
+                tipo = txbTipo.Text;
+                tamanho = txbTamanho.Text;
+                var dao = new ProdutoDAO();
+
+                if (Math.Abs(preco % 1) <= (Double.Epsilon * 100))
+                {
+                    MessageBox.Show("Represente virgula ao inves de ponto para representar" +
+                        "as casas decimais!", "Erro",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txbTipo.Focus();
+                    return;
+                }
+
+                if (dao.InsertProduto(nome, descricao, preco, tipo, tamanho))
+                {
+                    MessageBox.Show("Cadastrado com sucesso!", "Sucesso!",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void btnCadFunc_Click(object sender, EventArgs e)
+        {
+            string login;
+            string senha;
+            string tipo;
+
+            if (string.IsNullOrEmpty(txbLogin.Text) ||
+                string.IsNullOrEmpty(txbSenha.Text) ||
+                string.IsNullOrEmpty(txbTipoUsu.Text))
+            {
+                MessageBox.Show("Um dos campos está vazio, verifique e tente novamente!!","Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            else
+            {
+                login = txbLogin.Text;
+                senha = txbSenha.Text;
+                tipo = txbTipoUsu.Text;
+                var dao = new FuncionarioDAO();
+               if(dao.InsertUsuario(login, senha, tipo))
+                {
+                    MessageBox.Show("Usuario cadastrado com sucesso!", "Sucesso!",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+    }
+}
