373775f55b6fcdf1732190531ba930c52fce8078
Luis Carlos <luisinho0903@gmail.com>
2020-11-13 20:06:25 -0300

    Checkpoint para pull

---
+++ Model/Pizza.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderPizza.Model
+{
+    class Pizza
+    {
+        public int id;
+        public int idEstoque;
+        public int idProduto;
+        public double quantidade;
+    }
+}
