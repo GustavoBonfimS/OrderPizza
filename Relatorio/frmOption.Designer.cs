dfab0edcd8fbf70112386235055bc16fc7a96b68
GustavoBonfimS <gustavo.hbonfim@outlook.com>
2020-11-30 21:00:05 -0300

    relatorio label in options

--- Relatorio/frmOption.Designer.cs
+++ Relatorio/frmOption.Designer.cs
@@ -37,6 +37,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.txbTelefone = new System.Windows.Forms.TextBox();
             this.labelTelefone = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lbItems
@@ -50,7 +51,7 @@
             // cbOptions
             // 
             this.cbOptions.FormattingEnabled = true;
-            this.cbOptions.Location = new System.Drawing.Point(12, 42);
+            this.cbOptions.Location = new System.Drawing.Point(12, 53);
             this.cbOptions.Name = "cbOptions";
             this.cbOptions.Size = new System.Drawing.Size(121, 21);
             this.cbOptions.TabIndex = 1;
@@ -68,7 +69,7 @@
             // 
             // txbId
             // 
-            this.txbId.Location = new System.Drawing.Point(385, 43);
+            this.txbId.Location = new System.Drawing.Point(385, 54);
             this.txbId.Name = "txbId";
             this.txbId.Size = new System.Drawing.Size(100, 20);
             this.txbId.TabIndex = 3;
@@ -76,7 +77,7 @@
             // labelId
             // 
             this.labelId.AutoSize = true;
-            this.labelId.Location = new System.Drawing.Point(358, 46);
+            this.labelId.Location = new System.Drawing.Point(358, 57);
             this.labelId.Name = "labelId";
             this.labelId.Size = new System.Drawing.Size(21, 13);
             this.labelId.TabIndex = 4;
@@ -84,7 +85,7 @@
             // 
             // btnBuscar
             // 
-            this.btnBuscar.Location = new System.Drawing.Point(504, 40);
+            this.btnBuscar.Location = new System.Drawing.Point(504, 51);
             this.btnBuscar.Name = "btnBuscar";
             this.btnBuscar.Size = new System.Drawing.Size(75, 23);
             this.btnBuscar.TabIndex = 5;
@@ -95,7 +96,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 26);
+            this.label1.Location = new System.Drawing.Point(9, 37);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(61, 13);
             this.label1.TabIndex = 6;
@@ -103,7 +104,7 @@
             // 
             // txbTelefone
             // 
-            this.txbTelefone.Location = new System.Drawing.Point(234, 42);
+            this.txbTelefone.Location = new System.Drawing.Point(234, 53);
             this.txbTelefone.Name = "txbTelefone";
             this.txbTelefone.Size = new System.Drawing.Size(100, 20);
             this.txbTelefone.TabIndex = 7;
@@ -111,17 +112,28 @@
             // labelTelefone
             // 
             this.labelTelefone.AutoSize = true;
-            this.labelTelefone.Location = new System.Drawing.Point(179, 46);
+            this.labelTelefone.Location = new System.Drawing.Point(179, 57);
             this.labelTelefone.Name = "labelTelefone";
             this.labelTelefone.Size = new System.Drawing.Size(49, 13);
             this.labelTelefone.TabIndex = 8;
             this.labelTelefone.Text = "Telefone";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(82, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(475, 24);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Selecione por qual categoria deseja buscar os registros";
             // 
             // frmOption
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(591, 472);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.labelTelefone);
             this.Controls.Add(this.txbTelefone);
             this.Controls.Add(this.label1);
@@ -150,5 +162,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txbTelefone;
         private System.Windows.Forms.Label labelTelefone;
+        private System.Windows.Forms.Label label2;
     }
 }
